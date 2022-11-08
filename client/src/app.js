86a94c9839633e31fea7f0be14662afe65bbf3c4
Manny <mannysangha020@gmail.com>
2022-11-07 20:56:09 -0800

    added import components

--- client/src/app.js
+++ client/src/app.js
@@ -8,12 +8,12 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from '';
-import Signup from '';
-import Login from '';
-import Profile from '';
-import Header from '';
-import Footer from '';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
