cd26b05a19f9580f3c9bc8ff459d061ea5d3d9a8
aenz <andrewjensor@gmail.com>
2022-11-09 17:09:23 -0800

    added @apollo/client

--- devmound/src/App.js
+++ devmound/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import profile from './pages/Profile';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+    uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authlink = setContext((_, { Headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('id_token')
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...Headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const client = new ApolloClient({
+    // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+    link: authlink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <ApolloProvider client={client}>
+            <Router>
+                <div className="flex-colum justify-flex-start min-100-vh">
+                    <Header />
+                    <div className="container">
+                        <Routes>
+                            <Route
+                            path="/home"
+                            element={<Home />}
+                            />
+                            <Route 
+                            path="/login"
+                            element={<Login />}
+                            />
+                            <Route
+                            path="/signup"
+                            element={<Signup />}
+                            />
+                            <Route
+                            path="/profile"
+                            element={<Profile />}
+                            />
+
+                        </Routes>
+                    </div>
+                    <Footer />
+                </div>
+            </Router>
+        </ApolloProvider>
+    );
 }
 
 export default App;
